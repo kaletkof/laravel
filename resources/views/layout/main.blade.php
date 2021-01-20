bc1be09c80efb16476f36bb4bc7da5f5b44790d6
kaletkof <dev@krzysztof-kaletka.pl>
2021-01-20 17:14:34 +0100

    link css and js file

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -3,6 +3,10 @@
         <title>
             @yield('title', $applicationName)
         </title>
+
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <script src="{{ mix('js/app.js') }}"></script>
+
         <style>
             td {
                 padding-right: 30px;
