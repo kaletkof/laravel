1abd8c1c6251cd0ba9d1e39e32be940bde649e2a
kaletkof <dev@krzysztof-kaletka.pl>
2021-01-20 11:59:37 +0100

    conditional statements in blade

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -14,7 +14,16 @@
         <li>Imię: {{ $user['firstName'] }}</li>
         <li>Nazwisko: {{ $user['lastName'] }}</li>
         <li>Miasto: {{ $user['city'] }}</li>
-        <li>Wiek: {{ $user['age'] }}</li>
+        
+        <li>
+            Wiek: {{ $user['age'] }}
+            @if ($user['age'] >= 18)
+                <div>Osoba dorosła</div>
+            @else 
+                <div>Nastolatek</div>    
+            @endif    
+        </li>
+
     </ul>
     
     <div>
